ce9cdfb1f4fad0d98e35d5d7e23f09b3d424d297
label <label@gmail.com>
2017-05-17 14:16:50 +0800

    Initial commit

---
+++ app/src/androidTest/java/mattburg_coffee/labelbase/ApplicationTest.java
@@ -0,0 +1,13 @@
+package mattburg_coffee.labelbase;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
